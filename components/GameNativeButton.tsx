aa253d787b8e6d7599a214ced553c551e613ab8c
Fetteruruk <lukasmo.mosakul@gmail.com>
2026-09-08 12:00:26 +0200

    Pass persisted game credentials directly into native game view

--- components/GameNativeButton.tsx
+++ components/GameNativeButton.tsx
@@ -10,27 +10,19 @@ declare global {
   }
 }
 
-type Props = { accountId: string };
+type Props = { accountId: string; username?: string; password?: string };
 type Credential = { username: string; password: string };
 type ScriptEntry = { id:string; name:string; source:string; enabled:boolean; type:'Tampermonkey'|'Gist' };
 
 const GAME_URL = 'https://www.die-staemme.de/';
-const CREDENTIALS_KEY = 'odin-game-credentials';
 const SCRIPTS_KEY = 'odin-script-library';
 
-export default function GameNativeButton({ accountId }: Props) {
+export default function GameNativeButton({ accountId, username = '', password = '' }: Props) {
   const openGame = async () => {
     localStorage.setItem('odin-selected-game-account', accountId);
 
-    let username = '';
-    let password = '';
     let scriptsJson = '[]';
     try {
-      const rawCredentials = localStorage.getItem(CREDENTIALS_KEY);
-      const credentials = rawCredentials ? (JSON.parse(rawCredentials) as Record<string, Credential>) : {};
-      username = credentials[accountId]?.username ?? '';
-      password = credentials[accountId]?.password ?? '';
-
       const rawScripts = localStorage.getItem(SCRIPTS_KEY);
       const scripts = rawScripts ? (JSON.parse(rawScripts) as ScriptEntry[]) : [];
       scriptsJson = JSON.stringify(
