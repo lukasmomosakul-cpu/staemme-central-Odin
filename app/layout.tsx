238406247770349c8369535ef2b289675158e4d7
Fetteruruk <lukasmo.mosakul@gmail.com>
2026-09-09 12:30:33 +0200

    Fix version badge vertical position

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,5 +12,5 @@ export const metadata: Metadata = {
 export const viewport: Viewport = { width: 'device-width', initialScale: 1, viewportFit: 'cover' };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="de"><head><style>{`header.top > div > span.muted { display: none !important; } .mobileNav > span.muted { visibility:hidden !important; } #odin-version-updater { position:fixed; top:28px; right:28px; z-index:60; } @media(max-width:900px){#odin-version-updater{top:15px;right:16px}}`}</style></head><body>{children}<div id="odin-version-updater"><VersionUpdater /></div></body></html>;
+  return <html lang="de"><head><style>{`header.top > div > span.muted { display: none !important; } .mobileNav > span.muted { visibility:hidden !important; } #odin-version-updater { position:fixed; top:36px; right:28px; z-index:60; } @media(max-width:900px){#odin-version-updater{top:23px;right:16px}}`}</style></head><body>{children}<div id="odin-version-updater"><VersionUpdater /></div></body></html>;
 }
